2ffb77a51e3c627f08d47de3c1d379039d1e2253
Carlos Sicre Vara de Rey <sicre.carlos@gmail.com>
2014-11-20 21:30:06 +0100

    añadir vistas

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,28 @@
 class ProductsController < ApplicationController
+
+def new
+end
+
+def create
+  @product = Product.new(product_params)
+
+  @product.save
+  redirect_to @product
+end
+
+def show
+  @product = Product.find(params[:id])
+end
+
+def index
+  @products = Product.all
+end
+
+private
+  def product_params
+    params.require(:product).permit(:name, :weight)
+  end
+
+
+
 end
