1da0809e98f7693258fb7fae616a004eed0d76d8
Carlos Sicre Vara de Rey <sicre.carlos@gmail.com>
2014-11-20 18:18:46 +0100

    Limitar el número de caracteres del nombre del producto

--- db/migrate/20141120165525_create_products.rb
+++ db/migrate/20141120165525_create_products.rb
@@ -1,8 +1,8 @@
 class CreateProducts < ActiveRecord::Migration
   def change
     create_table :products do |t|
-      t.string :name
-      t.integer :weight
+      t.string :name, :limit => 150
+      t.integer :weight, :default => 0
 
       t.timestamps
     end
